e889600b5ab6c95db25131f970fe2a9a20083de1
Don MacKinnon <dmackinn@revolutionlabs.com>
2014-09-21 11:42:57 -0600

    Changed deprecated "deps" package dependency to "tracker"

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse( function(api) {
     api.versionsFrom("METEOR@0.9.0");
     api.use('accounts-base');
-    api.use(['deps'], 'client');
+    api.use(['tracker'], 'client');
     api.add_files(['client.js'], 'client');
     api.add_files(['server.js'], 'server');
     api.export('UserLoginState');
