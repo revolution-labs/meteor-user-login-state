44bb4649f6a8868014739b54c7dfd97355f3f92c
Don MacKinnon <dmackinn@revolutionlabs.com>
2014-09-21 11:42:57 -0600

    Changed deprecated "deps" package dependency to "tracker"

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
     name: "revolutionlabs:user-login-state",
     summary: "Monitor current user log in and log out activity and provide callback hooks for these events.",
-    version: "1.0.0",
+    version: "1.0.1",
     git: "https://github.com/revolution-labs/meteor-user-login-state.git"
 });
 
 Package.onUse( function(api) {
     api.versionsFrom("METEOR@0.9.0");
     api.use('accounts-base');
-    api.use(['deps'], 'client');
+    api.use(['tracker'], 'client');
     api.add_files(['client.js'], 'client');
     api.add_files(['server.js'], 'server');
     api.export('UserLoginState');
